bec781e1b8f466a3c530d9b96eb10bc6ee71d9e4
nglow <nglow90@gmail.com>
2021-06-18 14:11:21 +0900

    Update URI of all API

--- src/main/java/blowest/kiosk/controller/MenuTypeController.java
+++ src/main/java/blowest/kiosk/controller/MenuTypeController.java
@@ -26,35 +26,35 @@ public class MenuTypeController {
     }
 
 
-    @GetMapping("/menutypes/test")
+    @GetMapping("/menu_types/test")
     public String test(){return "Test";}
 
-    @PostMapping("/menutypes")
+    @PostMapping("/menu_types")
     public Long create(@RequestBody MenuTypeRequestDto request){
         return menuTypeService.createMenuType(request);
     }
 
-    @GetMapping("/menutypes")
+    @GetMapping("/menu_types")
     public List<MenuTypeResponseDto> findAll(){
         return menuTypeService.retrieveAllMenuTypes();
     }
 
-    @GetMapping("/menutypes/{id}")
+    @GetMapping("/menu_types/{id}")
     public MenuTypeResponseDto findMenuType(@PathVariable Long id){
         return menuTypeService.retrieveMenuType(id);
     }
 
-    @PatchMapping("/menutypes/{id}")
+    @PatchMapping("/menu_types/{id}")
     public Long updateMenuType(@PathVariable Long id, @RequestBody MenuTypeRequestDto requestDto){
         return menuTypeService.updateMenuType(id, requestDto);
     }
 
-    @DeleteMapping("/menutypes/{id}/deactivate")
+    @DeleteMapping("/menu_types/{id}/deactivate")
     public void deactivateMenuType(@PathVariable Long id){
         menuTypeService.deactivateMenuType(id);
     }
 
-    @PatchMapping("/menutypes/{id}/activate")
+    @PatchMapping("/menu_types/{id}/activate")
     public void activateMenuType(@PathVariable Long id){
         menuTypeService.activateMenuType(id);
     }
